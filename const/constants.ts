fce4b562a660c2655ebec19729e0daed2c79a84e
sunagi <akaractionend@icloud.com>
2023-06-18 00:38:25 +0900

    fix2_registry

--- const/constants.ts
+++ const/constants.ts
@@ -1,7 +1,7 @@
 import { Astar, Chain } from '@thirdweb-dev/chains'
 // your token bound factory address
-export const factoryAddress: string = '0xE7a2b16309CDe1E4ba77A80e7d9FCa1af546099B'
-export const implementation: string = '0x06381210493b9A3617ed1cD268e440ed53Bf45BC'
+export const factoryAddress: string = '0x5B0Dda047f59B8fE54BbAb5FFcd2AF7E309B0979'
+export const implementation: string = '0xdAb37ef81EE81769Dc8b76101d1b2A76140c3a48'
 export const TWApiKey: string = '0312f251a35ad87e43ba115135bd16b188feb58808ae17f69573e74d1416ee4b4c41d62019b8037cffbbef83966b5391ff9944b788b3434b94105c1a6a477874'
 export const activeChain: Chain = Astar
 export const nftDropAddress: string = '0x9bD8c75992e6Ef01BE2874e90CdD0a0B44825918'
