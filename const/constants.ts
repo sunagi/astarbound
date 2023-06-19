e0501d49a7618325c82175da054b97f47b9ab278
suna <akaractionend@icloud.com>
2023-06-19 22:13:12 +0900

    change_entry

--- const/constants.ts
+++ const/constants.ts
@@ -1,9 +1,9 @@
 import { Astar, Chain } from '@thirdweb-dev/chains'
 // your token bound factory address
 export const factoryAddress: string = '0x449dEC5c586021540950BFa3dB823d930CD97d07'
-export const implementation: string = '0x42C8d4341Cbbd248C3c8577E27eFA7719D9dA342'
+export const implementation: string = '0xBDAF88Af8c68940436c13e64e27512cd68D7b087'
 export const TWApiKey: string = '0312f251a35ad87e43ba115135bd16b188feb58808ae17f69573e74d1416ee4b4c41d62019b8037cffbbef83966b5391ff9944b788b3434b94105c1a6a477874'
 export const activeChain: Chain = Astar
-export const nftDropAddress: string = '0x9bD8c75992e6Ef01BE2874e90CdD0a0B44825918'
-export const tokenAddress: string = '0xCD7A24Fd081562FD5a0dE8B79B6efaC41187b3d7'
-export const licenseAddress: string = '0x36043e314430bc1B236B028C2b655B1a410FC2A3'
+export const nftDropAddress: string = '0x8A75AC2d75E0E930032EAD701d5f13a865499890'
+export const tokenAddress: string = '0xda88328Ec6D109cb84058D95FD1d956d85B5d724'
+export const licenseAddress: string = '0xe5d84E63d8527E08261A39C4239Ff4f72629346B'
